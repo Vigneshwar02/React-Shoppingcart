8546d2b8ed706dfdce1267b747793522c977ff15
PrismLight02 <blazingvickyz@gmail.com>
2022-08-20 18:06:27 +0530

    cors error

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ const cors = require('cors');
 const List = require("./model/mongo");
 const Cart = require('./model/cart')
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
-
 
 
 app.get('/',(req,res)=>{
