04828f7b27a491069ae5352bdf5b65f85a30bd76
PrismLight02 <blazingvickyz@gmail.com>
2022-08-20 18:02:39 +0530

    cors error

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const List = require("./model/mongo");
 const Cart = require('./model/cart')
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Origin", '*', "https://my-custom-react-shoppingcart.herokuapp.com");
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
